a94a2969198a66a8af624ee298a09343eef03f37
liozhu <850717623@qq.com>
2023-08-03 21:10:59 +0800

    Day18

--- src/main/java/easy/T83删除排序链表中的重复元素.java
+++ src/main/java/easy/T83删除排序链表中的重复元素.java
@@ -1,5 +1,7 @@
 package easy;
 
+import common.ListNode;
+
 public class T83删除排序链表中的重复元素 {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
@@ -34,17 +36,5 @@ public class T83删除排序链表中的重复元素 {
             }
         }
         return head;
-    }
-
-    static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) {
-            this.val = val;
-        }
-        ListNode(int val, ListNode next) {
-            this.val = val; this.next = next;
-        }
     }
 }
